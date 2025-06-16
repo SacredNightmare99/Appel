cfa72ab7bc90da043b9dd6c029f5fe4d83adc81a
Ishaan Jindal <ishaanjindal2006@gmail.com>
2025-06-16 18:50:36 +0530

    redoing everything {fresh start}

--- lib/app/classes/student.dart
+++
@@ -1,6 +0,0 @@
-class Student {
-  final String name;
-  bool present;
-
-  Student({required this.name, this.present = false});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:the_project/utils/routes.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,11 +22,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "Attendance LFWA",
-      
-      home: navBarRoute,
-
+      debugShowCheckedModeBanner: false,
     );
   }
 }
